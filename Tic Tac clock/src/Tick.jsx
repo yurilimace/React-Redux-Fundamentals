e9982094675901e0b3af0542cf13a8976cde4f1f
José Yuri <yurilima.ce@gmail.com>
2019-02-25 18:28:54 -0300

    implementing Clock class with state and lifecycle method

--- Tic Tac clock/src/Tick.jsx
+++ Tic Tac clock/src/Tick.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
 
+export default class Clock extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {date: new Date()};
+    }
+    /*lifecycle methods, when the component is render in DOM this method(Mount) and run the
+    code inside it*/
+    
+    componentDidMount(){
+        setInterval(
+            ()=> this.tick(),1000
+        )
+    }
+    
+    /*opsite the mount method, unmount run the code after the component been remove from the DOM*/
+    componentWillUnmount(){
+        clearInterval(this.TimerId)
+    }
 
-export default function tick(props){
-    const element = (
-        <div>
-            <h1>Tic tac clock</h1>
-            <h2> It is {props.date.toLocaleTimeString()}</h2>
-        </div>
-    )
-   return element
+    //method that updates the state of the component every 1 second
+    tick(){
+        this.setState({
+            date: new Date()
+        })
+    }
 
+    render(){
+        return(
+            <div>
+                <h1>Hello, World</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+            </div>
+        );    
+    }
 }
 

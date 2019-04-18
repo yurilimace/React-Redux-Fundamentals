e7dd16bba3d4f1d061c016be0e97bbab8a59efc9
José Yuri <yurilima.ce@gmail.com>
2019-04-18 10:05:47 -0300

    table cell fixed size item and alignment

--- check-reading/src/Components/table.jsx
+++ check-reading/src/Components/table.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Fileinput from './fileInput'
+import Button from './buttons'
 export default props =>{
   
   const list = props.list || []
@@ -7,9 +7,15 @@ export default props =>{
     return(
       list.map((item,index) =>{
         return(
-            <tr key={index}>
-              <td> <img src={item[1]} height='250' width='200'/></td>
-              <td> {item[0]} </td>
+            <tr key={index} scope='row'>
+                <td  className='cellsize align-middle' align='center'> <span> <img  src={item[1]} height='250' width='200' /> </span> </td>
+                <td className='cellsize align-middle'align='center'> {item[0]} </td>
+                <td className='cellsize align-middle' align='center'> 
+                  <Button btn= 'btn btn-success btn-sm'  icon ='check' handleClick = {props.handleClick}></Button><br/>
+                  <Button btn= 'btn btn-danger btn-sm'  icon ='trash' handleClick = {props.handleClick}></Button><br/>
+                  <Button btn= 'btn btn-info btn-sm'  icon ='thumbs-up' handleClick = {props.handleClick}></Button><br/>
+                  <Button btn= 'btn btn-warning btn-sm'  icon ='undo' handleClick = {props.handleClick}></Button><br/>
+                </td>
             </tr>
         )
        }
@@ -20,11 +26,12 @@ export default props =>{
   
   return(
         <div className = "col-xl-6 offset-3">
-            <table className='table'>
-              <thead>
+            <table className='table table-sm'>
+              <thead align='center'>
               <tr>
                 <th scope="col">Cover</th>
                 <th scope="col">Tittle</th>
+                <th scope="col">Action</th>
               </tr>
               </thead>
               <tbody>
--- check-reading/src/index.js
+++ check-reading/src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import Route from './Routes/router'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBook,faPlus,faTrash,faSearch,faImage} from '@fortawesome/free-solid-svg-icons'
+import { faBook,faPlus,faTrash,faSearch,faImage,faCheck,faThumbsUp,faUndo} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faBook,faPlus,faTrash,faSearch,faImage)
+library.add(faBook,faPlus,faTrash,faSearch,faImage,faCheck,faThumbsUp,faUndo)
 
 
 ReactDOM.render(<Route />, document.getElementById('root'));
